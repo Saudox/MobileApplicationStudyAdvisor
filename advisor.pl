1554d0a16b100102bef5d3f14f365db3d347a65b
Bigad Elsayed Ramadan Omar <es.begad.alsayed2023@alexu.edu.eg>
2026-05-12 13:39:58 +0300

    some modifications on advisor.pl and refactoring

--- advisor.pl
+++ advisor.pl
@@ -11,7 +11,6 @@
 % course(id, display_name, difficulty, level)
 
 % Level 0
-
 course(math1,'Mathematics 1',easy,0).
 course(mechanics1,'Mechanics 1',easy,0).
 course(physics1,'Physics 1',easy,0).
@@ -23,9 +22,10 @@ course(physics2,'Physics 2',easy,0).
 course(eng_drawing2,'Engineering Drawing 2',easy,0).
 course(eng_chemistry,'Engineering Chemistry',easy,0).
 course(history_eng,'History of Engineering & Technology',easy,0).
-course(computer_society1,'Computer & Society 1',easy,0).
+
 
 % Level 1
+course(computer_society1,'Computer & Society 1',easy,1).
 course(linear_algebra,'Linear Algebra',medium,1).
 course(probability,'Probability & Computer Applications',medium,1).
 course(programming1,'Programming 1',easy,1).
@@ -37,6 +37,7 @@ course(data_structures,'Data Structures',medium,1).
 course(digital_systems,'Digital Systems Design',medium,1).
 course(electronics,'Electronics & Circuits',medium,1).
 course(computer_society2,'Computer & Society 2',easy,1).
+
 
 % Level 2
 course(numerical_analysis,'Numerical Analysis',hard,2).
@@ -52,6 +53,7 @@ course(control_systems,'Control Systems Fundamentals',hard,2).
 course(operating_systems,'Operating Systems',hard,2).
 course(technical_writing,'Technical Writing',easy,2).
 
+
 % Level 3
 course(algorithms,'Algorithm Design & Analysis',hard,3).
 course(networks,'Computer Networks',hard,3).
@@ -63,6 +65,7 @@ course(digital_control,'Digital Control & Modern Systems',hard,3).
 course(embedded_systems,'Embedded Systems',hard,3).
 course(computer_arch,'Computer Architecture',hard,3).
 course(critical_thinking,'Critical Thinking',easy,3).
+
 
 
 %%%%%%% Category facts %%%%%%%
@@ -78,6 +81,7 @@ category(statistical_methods,math).
 category(numerical_analysis,math).
 category(operations_research,math).
 
+
 % programming
 category(programming1,programming).
 category(data_structures,programming).
@@ -85,6 +89,7 @@ category(programming2,programming).
 category(prog_paradigms,programming).
 category(algorithms,programming).
 category(software_eng,programming).
+
 
 % hardware
 category(electrical_eng,hardware).
@@ -97,17 +102,21 @@ category(digital_control,hardware).
 category(embedded_systems,hardware).
 category(computer_arch,hardware).
 
+
 % theory
 category(discrete_structures,theory).
 category(ds_algorithms,theory).
 
+
 % ai
 category(intro_ai,ai).
+
 
 % systems
 category(databases,systems).
 category(operating_systems,systems).
 category(networks,systems).
+
 
 % general
 category(mechanics1,general).
@@ -145,11 +154,13 @@ prerequisite(differential_eq,math2).
 prerequisite(statistical_methods,math2).
 prerequisite(computer_society2,computer_society1).
 
+
 prerequisite(differential_eq,linear_algebra).
 prerequisite(statistical_methods,probability).
 prerequisite(data_structures,programming1).
 prerequisite(digital_systems,logical_design).
 prerequisite(electronics,electrical_eng).
+
 
 prerequisite(hci, programming1).
 prerequisite(numerical_analysis,differential_eq).
@@ -157,11 +168,13 @@ prerequisite(discrete_structures,data_structures).
 prerequisite(programming2,data_structures).
 prerequisite(computer_org,digital_systems).
 
+
 prerequisite(operations_research,numerical_analysis).
 prerequisite(ds_algorithms,discrete_structures).
 prerequisite(prog_paradigms,programming2).
 prerequisite(control_systems,computer_org).
 prerequisite(operating_systems,control_systems).
+
 
 prerequisite(algorithms,operations_research).
 prerequisite(algorithms,ds_algorithms).
@@ -169,6 +182,7 @@ prerequisite(networks,ds_algorithms).
 prerequisite(software_eng,prog_paradigms).
 prerequisite(intro_ai,control_systems).
 prerequisite(databases,operating_systems).
+
 
 prerequisite(digital_control,algorithms).
 prerequisite(embedded_systems,networks).
@@ -184,9 +198,15 @@ prerequisite_met(Student , Course) :-
     \+ ( prerequisite( Course , Pre ) , \+ student_completed( Student , Pre )).
 
 
+
+
+
 %%%%%%% course not taken rule %%%%%%%
 not_taken_yet(Student , Course) :-
     \+( student_completed( Student , Course )).
+
+
+
 
 
 %%%%%%% eligible rule %%%%%%%
@@ -194,9 +214,15 @@ eligible(Student , Course) :-
     course(Course, _, _, _) , prerequisite_met(Student , Course) , not_taken_yet(Student , Course).
 
 
+
+
+
 %%%%%%% student interest rule %%%%%%%
 student_interest(Student , Course) :-
     category(Course , Category) , love(Student , Category).
+
+
+
 
 %%%%%%% difficulty readiness rule %%%%%%%
 ready_for_difficulty(_, Course) :-
@@ -214,34 +240,10 @@ ready_for_difficulty(Student, Course) :-
         course(SomeCourse, _ , hard , _)
     )).
 
+
+
+
+
 %%%%%%% final rule %%%%%%%
 recommend(Student, Course) :-
     eligible(Student , Course) , student_interest(Student , Course) , ready_for_difficulty(Student, Course).
-
-
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%% FACTS %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% course(id, display_name, difficulty, level)
-% - Static data, no need to change
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%% RULES %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% - All rules are defined above, no need to change
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%% USAGE %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% > DJANGO:
-%   - Before querying, assert student data using:
-%       student_completed(StudentID, CourseID)
-%       love(StudentID, Category)
-%   - Query using: recommend(StudentID, Course)
-%   - After query, clean up asserted facts using : 
-%       retractall(student_completed(StudentID, _))
-%       retractall(love(StudentID, _))
-%
-% > MOBILE APP:
-%   - Send student completed courses and interests to Django
-%   - Interest options are: math, programming, hardware, theory, ai, systems , general
-%   - Display the course list returned from Django
